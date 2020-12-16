b923d42cd930c0fe235c60443fa51e1d0c2ddda5
Fomagran <fomagran6@naver.com>
2020-12-16 12:02:42 +0900

    imageView넣고 스크롤할때 페이드인 페이드아웃 효과주기

--- Onboarding Travel/Controller/FashionViewController.swift
+++ Onboarding Travel/Controller/FashionViewController.swift
@@ -10,21 +10,43 @@ import UIKit
 class FashionViewController: UIViewController {
     
     //MARK:Properties
+    @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var collection: UICollectionView!
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var nextBtn: UIButton!
     private var items:[OnboardingItem] = []
+    private var imageViews = [UIImageView]()
+    var collectionViewWidth:CGFloat {
+        return collection.frame.size.width
+    }
     //MARK:LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpCollectionView()
         setUpItems()
         setUpPageControl()
-        
+        setUpImageView()
     }
     
     //MARK:Helpers
     
+    //이미지뷰 넣기
+    private func setUpImageView() {
+        items.forEach { item in
+            let imageView = UIImageView(image: item.bgImage)
+            imageView.contentMode = .scaleAspectFill
+            imageView.alpha = 0.0
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            imageView.clipsToBounds = true
+            containerView.addSubview(imageView)
+            NSLayoutConstraint.activate([imageView.heightAnchor.constraint(equalTo: containerView.heightAnchor, multiplier: 0.8),
+                                         imageView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),imageView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),imageView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor)])
+            imageViews.append(imageView)
+            imageViews.first?.alpha = 1
+        }
+        //??뭐지 컬렉션뷰 앞에다가 넣는다는 뜻인가? - new
+        containerView.bringSubviewToFront(collection)
+    }
     private func setUpPageControl() {
         pageControl.numberOfPages = items.count
     }
@@ -45,6 +67,26 @@ class FashionViewController: UIViewController {
     //현재 인덱스 알아내기 -> new
     private func getCurrentIndex() -> Int {
         return Int(collection.contentOffset.x/collection.frame.width)
+    }
+    
+    //MARK: ScrollView Functions
+    
+    //컬렉션뷰 스크롤 했을 때도 넥스트버튼 누른 것처럼 똑같이 하기
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let index = getCurrentIndex()
+        showItem(at: index)
+    }
+    
+    //페이드인 페이드 아웃 코드로 넣어주기
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let x = scrollView.contentOffset.x
+        let index = getCurrentIndex()
+        let fadeInAlpha = (x - collectionViewWidth * CGFloat(index)) / collectionViewWidth
+        let fadeOutAlpha = CGFloat(1-fadeInAlpha)
+        let canShow = index < items.count - 1
+        guard canShow else {return}
+        imageViews[index].alpha = fadeOutAlpha
+        imageViews[index + 1].alpha = fadeInAlpha
     }
     
     //MARK:Actions
--- Onboarding Travel/Model/OnboardingItem.swift
+++ Onboarding Travel/Model/OnboardingItem.swift
@@ -21,9 +21,9 @@ struct OnboardingItem {
     ]
     
     static let fashionItems:[OnboardingItem] = [
-        .init(title: "Alexander McQueen", detail: "I think there is beauty in everything. What 'normal' people perceive as ugly, I can usually see something of beauty in it.", bgImage: #imageLiteral(resourceName: "imTravel1")),
-        .init(title: "Alexander Wang", detail: "Anyone can get dressed up and glamorous, but it is how people dress in their days off that are the most intriguing.", bgImage: #imageLiteral(resourceName: "imTravel3")),
-        .init(title: "Diana Vreeland", detail: "You gotta have style. It helps you get down the stairs. It helps you get up in the morning. It’s a way of life. Without it, you’re nobody. I’m not talking about lots of clothes.", bgImage: #imageLiteral(resourceName: "imTravel2")),
-        .init(title: "Hubert de Givenchy", detail: "The dress must follow the body of a woman, not the body following the shape of the dress.", bgImage: #imageLiteral(resourceName: "imTravel4"))
+        .init(title: "Alexander McQueen", detail: "I think there is beauty in everything. What 'normal' people perceive as ugly, I can usually see something of beauty in it.", bgImage: #imageLiteral(resourceName: "imFashion1")),
+        .init(title: "Alexander Wang", detail: "Anyone can get dressed up and glamorous, but it is how people dress in their days off that are the most intriguing.", bgImage: #imageLiteral(resourceName: "imFashion4")),
+        .init(title: "Diana Vreeland", detail: "You gotta have style. It helps you get down the stairs. It helps you get up in the morning. It’s a way of life. Without it, you’re nobody. I’m not talking about lots of clothes.", bgImage: #imageLiteral(resourceName: "imFashion2")),
+        .init(title: "Hubert de Givenchy", detail: "The dress must follow the body of a woman, not the body following the shape of the dress.", bgImage: #imageLiteral(resourceName: "imFashion3"))
     ]
 }
